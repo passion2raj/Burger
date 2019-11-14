8faf00745086e7edde9a12dc684d5a293c1a447f
Rajesh Rana <asra8175@gmail.com>
2019-11-13 19:23:32 -0500

    Devoured working

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+INSERT INTO burgers(burger_name, devoured,createdAt)
+VALUES ("hamburger",false,0), ("cheeseburger",false,0), ("veggieburger",false,0);
+
+
---
+++ public/assets/js/burger.js
@@ -0,0 +1,53 @@
+$(function() {
+    $("#addburger").on("click", function(event) {
+      event.preventDefault();
+  
+      var newBurger = {
+        burger_name: $("#newburger")
+          .val()
+          .trim(),
+        devoured: 0
+      };
+      console.log(newBurger)
+
+
+  
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+      }).then(function() {
+        console.log("Added new burger",);
+        location.reload();
+      });
+    });
+  
+    $(".eatburger").on("click", function(event) {
+      event.preventDefault();
+  
+      var id = $(this).data("id");
+      var devouredState = {
+        devoured: 1
+      };
+  
+      $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+        data: devouredState
+      }).then(function() {
+        console.log("Burger devoured");
+        location.reload();
+      });
+    });
+  
+    $(".trashburger").on("click", function(event) {
+      event.preventDefault();
+  
+      var id = $(this).data("id");
+  
+      // Send the DELETE request.
+      $.ajax({
+        type: "DELETE",
+        url: "/api/burgers/" + id
+      }).then(location.reload());
+    });
+  });
+  
